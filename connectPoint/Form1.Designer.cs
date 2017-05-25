41d3f19a108a5ad0eced2bcc966548b9cf12618b
ZY-circle <sharon0919@126.com>
2017-05-25 18:36:24 +0800

    init

---
+++ connectPoint/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace connectPoint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(206, 215);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(402, 285);
+            this.axMapControl1.TabIndex = 0;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(1036, 607);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(27, 215);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(139, 63);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(217, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(912, 28);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(217, 137);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(912, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(29, 25);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(136, 48);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "打开";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(30, 117);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(136, 48);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1142, 671);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ connectPoint/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.DataSourcesFile;//ShapefileWorkspaceFactoryClass
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Carto;//IFeatureLayer
+using ESRI.ArcGIS.esriSystem;
+
+namespace connectPoint
+{
+    public partial class Form1 : Form
+    {
+        string shapeFileFullName = string.Empty;
+        string roadDataFullName = string.Empty;
+        //IPointCollection pPointColl;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+                IFeatureLayer pFeatureLayer = this.pLayer(roadDataFullName);
+                axMapControl1.AddLayer(pFeatureLayer);
+                axMapControl1.Refresh();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pOFD = new OpenFileDialog();
+            pOFD.Multiselect = false;
+            pOFD.Title = "打开路网文件";
+            pOFD.InitialDirectory = System.IO.Directory.GetCurrentDirectory();
+            pOFD.Filter = "路网文件(*.shp)|*.shp";
+            if (pOFD.ShowDialog() == DialogResult.OK)
+            {
+                roadDataFullName = pOFD.FileName;
+                this.textBox1.Text = roadDataFullName;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Shape文件（*.shp)|*.shp";
+            DialogResult dialogResult = saveFileDialog.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                shapeFileFullName = saveFileDialog.FileName;
+            }
+            else
+            {
+                shapeFileFullName = null;
+                return;
+            }
+            this.textBox2.Text = shapeFileFullName;
+        }
+        private IFeatureLayer pLayer(string roadDataFullName)//IPointCollection  GetAllPoint(string shapeFileFullName)
+        {
+            int index = roadDataFullName.LastIndexOf('\\');
+            string folder = roadDataFullName.Substring(0, index);
+            shapeFileFullName = roadDataFullName.Substring(index + 1);
+            IWorkspaceFactory pWSF = new ShapefileWorkspaceFactoryClass();
+            IFeatureWorkspace pFWS = (IFeatureWorkspace)pWSF.OpenFromFile(folder, 0);
+            
+            IFeatureClass pFC = pFWS.OpenFeatureClass(shapeFileFullName);
+            IFeatureCursor pFCursor = pFC.Search(null, false);
+            IFeature pFeature = pFCursor.NextFeature();
+            while (pFeature != null)
+            {
+
+                ITopologicalOperator pTO = pFeature.Shape as ITopologicalOperator;
+                IPointCollection pPointColl = new PolylineClass();
+                IPointCollection pPoints =  pTO.Boundary as IPointCollection;
+                pPointColl.AddPointCollection(pPoints );
+
+                IFeature pF = pFC.CreateFeature();
+                pF.Shape = pPointColl as IPolyline;//需要是个对象，如IPointCollection pPointColl = new PolylineClass();
+                pF.Store();
+              //  ICurve pCurve = new PolylineClass();
+              //  pCurve = pF as ICurve;
+              //  Console.WriteLine(pCurve.Length);
+                pFeature = pFCursor.NextFeature();
+            }
+            IFeatureLayer pFeatureLayer = new FeatureLayerClass();
+            pFeatureLayer.FeatureClass = pFC;
+            return pFeatureLayer;
+
+        }
+    }
+}
